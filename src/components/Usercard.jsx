585a1e5df55f488d3390ab781c0191022d585ef7
unknown <freejojaison555@gmail.com>
2024-04-26 15:21:43 +0530

    6

--- src/components/Usercard.jsx
+++ src/components/Usercard.jsx
@@ -1,39 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Usercard.css';
-import 'firebase/compat/auth';
-import userData from './userbook.json'; // make sure the path to the json file is correct
-import './download.png';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import userData from './userbook.json'; // Import the JSON file
+
 import Header from './Header';
+import QRCode from 'qrcode.react';
+
 
 const UserCard = () => {
-  const user = userData;
+  const [user, setUser] = useState(userData); // Use the imported data as initial state
+  const auth = getAuth();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, currentUser => {
+      if (currentUser) {
+        // User is signed in.
+        setUser(currentUser);
+      } else {
+        // No user is signed in.
+        setUser(null);
+      }
+    });
+
+    // Cleanup subscription on unmount
+    return () => unsubscribe();
+  }, [auth]);
+
+  if (!user) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className="user-card">
-         <Header />
-      <h2>{user.name}</h2>
-      <p>ID: {user.id}</p>
-      <div className='qr-code'><img src={require("./download.png")} alt="Login QR" /></div>
+      <Header />
+      <h2>{user.displayName}</h2>
+      <p>ID: {user.email}</p>
+      <div className='qr-code'>
+        <QRCode value={user.email} /> {/* Generate QR code */}
+      </div>
       
       <h3>List of allotted Books:</h3>
-      <table className='user-card table'>
-        <thead>
-          <tr>
-            <th>Name of Book</th>
-            <th>Date of Allot</th>
-            <th>Date of Return</th>
-          </tr>
-        </thead>
-        <tbody>
-          {user.books.map((book, index) => (
-            <tr key={index}>
-              <td>{book.name}</td>
-              <td>{book.allotDate}</td>
-              <td>{book.returnDate}</td>
+      {user.books ? (
+        <table className='user-card table'>
+          <thead>
+            <tr>
+              <th>Name of Book</th>
+              <th>Date of Allot</th>
+              <th>Date of Return</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {user.books.map((book, index) => (
+              <tr key={index}>
+                <td>{book.name}</td>
+                <td>{book.allotDate}</td>
+                <td>{book.returnDate}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p>No books found.</p>
+      )}
     </div>
   );
 };
